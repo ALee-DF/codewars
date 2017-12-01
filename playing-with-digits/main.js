5561ed7502d5cfc750d87a4b53a6017e44e6b1b8
Alexander Lee <alex8usa8@gmail.com>
2017-11-30 21:47:00 -0800

    Finished Playing with Digits Exericse.

---
+++ playing-with-digits/main.js
@@ -0,0 +1,19 @@
+function digPow(n, p){
+  const digits = n.toString().split('').map(digit => Number(digit))
+
+  let result = 0
+  for (let i = 0; i < digits.length; i++) {
+    result = result + Math.pow(digits[i], p + i)
+  }
+
+  let product = 0
+  let k = 0
+  while (product < result) {
+    if (result === (n * k)) {
+      return k
+    }
+    product = n * k
+    k++
+  }
+  return -1
+}
