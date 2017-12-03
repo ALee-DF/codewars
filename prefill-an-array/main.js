81d8fe22e53d3d8f2a3dac56cb6b64c0d5566c54
Alexander Lee <alex8usa8@gmail.com>
2017-12-02 18:48:37 -0800

    Finished Prefill An Array Exercise.

---
+++ prefill-an-array/main.js
@@ -0,0 +1,10 @@
+function prefill(n, v) {
+  let number = parseFloat(n)
+  if (Number.isInteger(number) && number >= 0) {
+    if (number === 0) return []
+    return [v, ...prefill(--number, v)]
+  }
+  else {
+    throw new TypeError(n + ' is invalid')
+  }
+}
